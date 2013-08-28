59215e6d6237dc08dd0561acc3ee60431d6f5965
tucci69 <igor.tan@gmail.com>
2013-08-28 17:21:20 +0100

    added threads

--- gunzip.cpp
+++ gunzip.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <thread>
+
 #include <boost/iostreams/filtering_streambuf.hpp>
 #include <boost/iostreams/copy.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
@@ -14,7 +16,6 @@ using namespace std;
 void gunzip( char* file_name)
 {
   using namespace boost::iostreams;
-  cout << file_name << endl;
   string tmp_file_name = string(file_name) + ".tmp";
   ifstream ifile(file_name,ios_base::in | ios_base::binary);
   filtering_streambuf<input> in;
@@ -33,6 +34,7 @@ void gunzip( char* file_name)
   ut.modtime=mtime;
   ut.actime=mtime;
   utime(orig_file_name.c_str(),&ut);
+  cout << orig_file_name << endl;
 }
 
 
@@ -41,8 +43,19 @@ int main( int argc, char* argv[] )
 {
 
   if(argc==1) return 0;
+
+  list<thread> threads;
   for_each( argv+1, argv+argc, 
-            [](char* elem ){ gunzip(elem) ; } 
+            [ &threads ]( char* elem )
+              { 
+                threads.push_back( thread( bind( gunzip, elem ) ) );
+              } 
+          );
+
+
+  for_each( threads.begin(),
+            threads.end(),
+            []( thread& elem ){ elem.join(); } 
           );
 
 }
